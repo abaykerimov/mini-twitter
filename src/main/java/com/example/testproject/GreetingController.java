16b8755554176f1c7fc02ab7ff2429107f0e7056
abaykerimov <abay-001@mail.ru>
2019-08-22 21:33:21 +0600

    Hibernate: on

--- src/main/java/com/example/testproject/GreetingController.java
+++ src/main/java/com/example/testproject/GreetingController.java
@@ -1,12 +1,21 @@
 package com.example.testproject;
 
+import com.example.testproject.domain.Message;
+import com.example.testproject.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
+    @Autowired
+    private MessageRepo messageRepo;
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
@@ -15,7 +24,29 @@ public class GreetingController {
 
     @GetMapping
     public String main(Model model) {
-        model.addAttribute("some", "SPRING!");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Model model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.addAttribute("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String filter, Model model) {
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
         return "main";
     }
 }
